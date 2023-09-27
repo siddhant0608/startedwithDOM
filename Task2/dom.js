7e3e89b84e9a810d9e72d921f0f61de26bdd0e90
siddhant0608 <siddhantlahane66>
2023-09-27 10:37:35 +0530

    GetElementsbyClassnameTask

---
+++ Task2/dom.js
@@ -0,0 +1,6 @@
+var itmes = document.getElementsByClassName('list-group-item');
+itmes[2].style.backgroundColor='green';
+
+for(var i=0;i<itmes.length;i++){
+    itmes[i].style.fontWeight ='bold';
+}
